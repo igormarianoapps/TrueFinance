bdf3a5ca9425f448a54f41f7088b1499b024e5d4
Igor <ux.igormariano@gmail.com>
2026-02-21 12:57:25 -0300

    ajustes supabase 6

--- supabase/functions/create-checkout-session/index.ts
+++ supabase/functions/create-checkout-session/index.ts
@@ -1,6 +1,5 @@
-import { serve } from 'https://deno.land/std@0.177.0/http/server.ts'
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
-import Stripe from 'https://esm.sh/stripe@14.23.0?target=deno'
+import Stripe from 'https://esm.sh/stripe@14.23.0?target=deno&deno-std=0.224.0'
 
 // Definimos o CORS aqui mesmo para evitar erros de importação
 const corsHeaders = {
@@ -8,7 +7,7 @@ const corsHeaders = {
   'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
 }
 
-serve(async (req) => {
+Deno.serve(async (req) => {
   // 0. Handle CORS (Pre-flight request)
   if (req.method === 'OPTIONS') {
     return new Response('ok', { headers: corsHeaders })
--- supabase/functions/stripe-webhook/index.ts
+++ supabase/functions/stripe-webhook/index.ts
@@ -1,6 +1,5 @@
-import { serve } from 'https://deno.land/std@0.177.0/http/server.ts'
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
-import Stripe from 'https://esm.sh/stripe@14.23.0?target=deno'
+import Stripe from 'https://esm.sh/stripe@14.23.0?target=deno&deno-std=0.224.0'
 
 const stripe = new Stripe(Deno.env.get('STRIPE_SECRET_KEY') as string, {
   apiVersion: '2023-10-16',
@@ -14,7 +13,7 @@ const supabaseAdmin = createClient(
 
 const webhookSecret = Deno.env.get('STRIPE_WEBHOOK_SECRET')
 
-serve(async (req) => {
+Deno.serve(async (req) => {
   // Array para guardar os logs e enviar de volta para o Stripe
   const logs: string[] = []
   const addLog = (msg: string) => {
